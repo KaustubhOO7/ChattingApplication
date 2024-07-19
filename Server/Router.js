228a630e08cbaa7c81ca54723888183ece79e19e
Kaustubh Sharma <sharma.kaustubh.05@gmail.com>
2024-07-19 17:45:21 +0530

    creating backend on render

--- Server/Router.js
+++ Server/Router.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`Connected to Port ${PORT}`);
 });
